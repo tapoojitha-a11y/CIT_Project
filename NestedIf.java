d9498ce406d74aff4d0fe70ba977fd372b512e8c
Poojitha T A <tapoojitha@gmail.com>
2026-03-26 12:13:30 +0530

    Updated nestedif structures

--- NestedIf.java
+++ NestedIf.java
@@ -10,8 +10,8 @@ public class NestedIf {
             if (age >= 65) {
                 System.out.println("Eligible to vote and avail govrernment fund");
             } else
-                System.out.println("We are not eligible to vote and avail government scheme");
+                System.out.println(" Eligible to vote and not eligible to avail government scheme");
         } else
-            System.out.println("We are not eligible to vote");
+            System.out.println(" Not Eligible to vote and not eligible to avail");
     }
 }
