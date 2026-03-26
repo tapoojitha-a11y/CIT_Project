30b64e27cc2731ecf6c2b35268e80868f6f9b6db
Poojitha T A <tapoojitha@gmail.com>
2026-03-26 14:16:54 +0530

    Updated JumpBreak Structures

---
+++ JumpBreak.java
@@ -0,0 +1,10 @@
+public class JumpBreak {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5)
+                // break;
+                continue;
+            System.out.println("Value is:" + i);
+        }
+    }
+}
