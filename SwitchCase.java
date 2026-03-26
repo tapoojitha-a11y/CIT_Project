d676afc04bb90e6be59732e330a097925f30aafc
Poojitha T A <tapoojitha@gmail.com>
2026-03-26 12:45:47 +0530

    Updated SwitchCase Structure

---
+++ SwitchCase.java
@@ -0,0 +1,26 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("Thrusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            default:
+                System.out.println("Invalid day given");
+
+        }
+
+    }
+}
