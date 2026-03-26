bd6e2a049f3ae1783134d30b886739741a5fb8d7
Poojitha T A <tapoojitha@gmail.com>
2026-03-26 14:33:28 +0530

    Update JumpReturn Structure

--- JumpReturn.java
+++ JumpReturn.java
@@ -2,6 +2,7 @@ public class JumpReturn {
     public static void main(String[] args) {
         System.out.println("Before return statement");
         // return;
-        System.out.println("Error after return statement");
+        // This is not allowed and gives error
+        // System.out.println("Error after return statement");
     }
 }
